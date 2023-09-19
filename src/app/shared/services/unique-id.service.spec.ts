4f379461cac2c1af0d7c0b89439536dd8d1a63e7
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-09-19 16:38:27 -0300

    test: add first test to generateUniqueID

--- src/app/shared/services/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id.service.spec.ts
@@ -2,15 +2,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
-  let service: UniqueIdService;
+describe(UniqueIdService.name, () => {
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({});
+  //   service = TestBed.inject(UniqueIdService);
+  // });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UniqueIdService);
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const service: UniqueIdService = new UniqueIdService();
+
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate id when called multiple times`, () => {
+    const service: UniqueIdService = new UniqueIdService();
   });
 });
